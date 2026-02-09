74287acf0b25005cf64d02329b50e72cde5303a4
Naga Venkata Sai Siva Varshith Chanda <varshithchanda91@gmail.com>
2026-02-09 11:48:31 +0530

    initial commit

---
+++ agricconnect-frontend/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <h1 className="tagline">Empowering Farmers for a Sustainable Future</h1>
+        <h2 className="subtitle">
+          Together, we can cultivate success and resilience through knowledge, innovation, and community
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
